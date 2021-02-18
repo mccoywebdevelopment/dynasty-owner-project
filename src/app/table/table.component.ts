05e38c1cdfbefc6bc89ffa9a43174691679db7d2
cmmccoy1996@gmail.com <cmmccoy1996@gmail.com>
2021-02-18 10:01:03 -0700

    The functionality of the project is complete.

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -7,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
+  @Input() data: any;
+
   constructor() { }
+
+  _getDateAndTime = (date) => {
+    let current = new Date(date);
+    let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+    let cTime = current.getHours() + ":" + current.getMinutes();
+    let dateTime = cDate + '\n' + cTime;
+
+    var hours = current.getHours();
+    var hours = (hours + 24 - 2) % 24;
+    var mid = 'am';
+    if (hours > 12) {
+      mid = 'pm';
+    }
+
+    return dateTime + " " + mid;
+  }
 
   ngOnInit() {
   }
